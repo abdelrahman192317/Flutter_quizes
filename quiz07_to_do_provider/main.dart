ad24309b622fdaa322da9f9b98e9766815c86927
abdo shaisha <abdoshaisha523@gmail.com>
2022-09-07 13:38:04 +0200

    Update main.dart

--- quiz07_to_do_provider/main.dart
+++ quiz07_to_do_provider/main.dart
@@ -1,1 +1,32 @@
+import 'package:flutter/material.dart';
 
+import 'package:provider/provider.dart';
+
+import 'homeScreen.dart';
+import 'toDoList.dart';
+
+void main() {
+  runApp(ChangeNotifierProvider(
+    create: (context) => ToDoList(),
+    child:MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
